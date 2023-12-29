b933a2ccc2f15132839bddf55dfe12c7d7749f1d
Graceboni <104657973+Graceboni@users.noreply.github.com>
2023-12-29 02:20:18 +0000

    work on slides

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,25 @@
-// Importing the necessary functions and classes from other modules.
-import { preloadImages } from './utils.js'; // Utility function to preload images
-import { Card1 } from './card.js';          // Class Card1 for data-effect hover-1
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+  // Apply to all slideshows that you define with the markup wrote
+  slideshows.forEach(initSlideShow);
 
+  function initSlideShow(slideshow) {
 
-// Array to hold the instantiated card objects.
-let cardsArr = [];
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
 
-// Query all non-empty card elements and for each card:
-[...document.querySelectorAll('.card:not(.card--empty)')].forEach(cardEl => {
-    // Find the closest parent grid element of the card and get its data-effect attribute value.
-    let gridEffect = cardEl.closest('.grid').getAttribute('data-effect');
+    var index = 0, time = 5000;
+    slides[index].classList.add('active');  
     
-    // Depending on the data-effect value, instantiate the appropriate Card class.
-    switch(gridEffect) {
-        case 'hover-1':
-            cardsArr.push(new Card1(cardEl));  // For data-effect hover-1, use Card1
-            break;
-        default:
-            console.warn('Unknown data-effect', gridEffect);
-            break;
-    }
-});
-    
-// Preload all images
-// Once all images are preloaded, remove the 'loading' class from the body.
-preloadImages('.card__img').then(() => document.body.classList.remove('loading'));
+    setInterval( () => {
+      slides[index].classList.remove('active');
+      
+      //Go over each slide incrementing the index
+      index++;
+      
+      // If you go over all slides, restart the index to show the first slide and start again
+      if (index === slides.length) index = 0; 
+      
+      slides[index].classList.add('active');
+
+    }, time);
+  }
